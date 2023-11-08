5bb33f09a2382951a79c6700e4faa61ec3fbbef0
makemyownlife <zhangyong7120180@163.com>
2023-11-08 14:11:43 +0800

    多模块信息

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+mod api;
+use api::user::User;
+
 fn main() {
     println!("Hello, world!");
+    let u1 = User::new_user(String::from("tom"), 5);
+    println!("user name: {}", u1.name());
+    println!("1+2: {}", api::user::add(1, 2));
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        println!("Hello, mytest!");
+        assert_eq!(2 + 2, 4);
+    }
 }
