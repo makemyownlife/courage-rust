80c8e46268ca9158291aec05594d650614de0904
makemyownlife <zhangyong7120180@163.com>
2023-11-09 12:38:36 +0800

    字符串影响

--- src/main.rs
+++ src/main.rs
@@ -15,4 +15,67 @@ mod tests {
         println!("Hello, mytest!");
         assert_eq!(2 + 2, 4);
     }
+
+    #[test]
+    // 切片想要包含 String 的最后一个字节
+    fn slice_last() {
+        let s = String::from("hello");
+
+        let slice = &s[0..2];
+        println!("slice {}", slice);
+
+        let slice = &s[..2];
+        println!("slice {}", slice);
+    }
+
+    #[test]
+    // 截取完整的 String 切片
+    fn slice_all() {
+        let s = String::from("hello");
+
+        let len = s.len();
+
+        println!("len {}", len);
+
+        let slice = &s[0..len];
+        let slice = &s[..];
+        println!("slice {}", slice);
+
+    }
+
+    #[test]
+    // 截取完整的 String 切片
+    fn slice_index_error() {
+        let s = "中国人";
+        let a = &s[0..2];
+      //  println!("{}",a);
+    }
+
+    #[test]
+    // 截取完整的 String 切片
+    fn slice_mut() {
+        let mut s = String::from("hello world");
+
+        let word = first_word(&s);
+
+        s.clear(); // error!
+
+        // 请注意：假如去掉注解，则编译失败，\
+       // println!("the first word is: {}", word);
+    }
+
+    fn first_word(s: &String) -> &str {
+        &s[..1]
+    }
+
+    #[test]
+    // 截取完整的 String 切片
+    fn slice_other() {
+        let a = [1, 2, 3, 4, 5];
+
+        let slice = &a[1..3];
+
+        assert_eq!(slice, &[2, 3]);
+    }
+
 }
