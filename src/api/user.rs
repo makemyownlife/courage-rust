7e54036ed240b2145f3739cf7c74abcfb57da118
zhangyong <zhangyong7120180@163.com>
2023-11-12 11:11:04 +0800

    增加元组的单元测试

--- src/api/user.rs
+++ src/api/user.rs
@@ -119,5 +119,20 @@ mod tests {
         dbg!(string_clear);
     }
 
+    #[test]
+    // string 切片的 push 方法 字符串变量必须由 mut 关键字修饰。
+    fn tuple_test() {
+        let s1 = String::from("hello");
+
+        let (s2, len) = calculate_length(s1);
+
+        println!("The length of '{}' is {}.", s2, len);
+    }
+
+    fn calculate_length(s: String) -> (String, usize) {
+        let length = s.len(); // len() 返回字符串的长度
+        (s, length)
+    }
+
 
 }
